668122a9f151721926a3671ef646b5c2a704a09a
heungseok <heungseok2@gmail.com>
2020-02-21 22:24:24 +0900

    testing gh page

---
+++ precache-manifest.472cf50096aafdde39853dd6d0cd4bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3606591201acbd386523932865170df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6874a5921bb4a2d4598",
+    "url": "/static/css/main.6c9d3c3f.chunk.css"
+  },
+  {
+    "revision": "74038f147fe0dda3bcd3",
+    "url": "/static/js/2.9fcce470.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.9fcce470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6874a5921bb4a2d4598",
+    "url": "/static/js/main.ab2a2a21.chunk.js"
+  },
+  {
+    "revision": "2beabfaaed16c3a4364a",
+    "url": "/static/js/runtime-main.6761561c.js"
+  }
+]);
